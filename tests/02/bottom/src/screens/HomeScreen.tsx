35f3ae96d8b743d046ce79df98aacd81d7a78580
André de Paula Terceiro <andredepaula@comerciarios.org.br>
2025-12-01 09:26:38 -0300

    test: Creating two screens in the test app

---
+++ tests/02/bottom/src/screens/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import { StatusBar } from "expo-status-bar";
+import { View, Text } from 'react-native';
+
+export default function HomeScreen() {
+    return (
+        <View style={{flex: 1, backgroundColor: 'blue'}}>
+            <Text>Home works!!</Text>
+        </View>
+    )
+}
